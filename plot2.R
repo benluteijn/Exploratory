c0a5cec58439673396c809e003b9f58e5f4a0f6f
benluteijn <bmvaconsultancy@outlook.com>
2015-06-07 22:01:23 +0200

    Exploratory
    Exploratory

---
+++ plot2.R
@@ -0,0 +1,14 @@
+data<- read.table("household_power_consumption.txt", sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings= "?")
+
+# Subset the data 
+data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
+# Change date format
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+
+# Plot2
+png("plot2.png", width=480, height= 480)
+
+plot(data$DateTime, data$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="")
+
+dev.off()
